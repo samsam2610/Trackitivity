ed2f6193008bda0a9054b44ba5b078f513708f20
Sam Tran <quocsam93@gmail.com>
2017-11-30 20:25:30 -0500

    Initialize

---
+++ build/Pods.build/Debug-iphoneos/ResearchKit.build/DerivedSources/ResearchKit_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char ResearchKitVersionString[];
+ extern const double ResearchKitVersionNumber;
+
+ const unsigned char ResearchKitVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ResearchKit  PROJECT:Pods-1" "\n";
+ const double ResearchKitVersionNumber __attribute__ ((used)) = (double)1.;
